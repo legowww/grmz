ad94341408cdcac0c48139233d739b3c685c4248
PHJ <legowww@gmail.com>
2025-01-04 20:05:25 +0900

    feat: rest docs + swagger Open API

--- src/main/java/com/company/groomingzone/reservation/controller/request/ReservationCreateRequest.java
+++ src/main/java/com/company/groomingzone/reservation/controller/request/ReservationCreateRequest.java
@@ -2,6 +2,7 @@ package com.company.groomingzone.reservation.controller.request;
 
 import com.company.groomingzone.reservation.domain.ReservationTarget;
 
+
 public record ReservationCreateRequest(
         Long barberShopId,
         Long scheduleId,
---
+++ src/test/java/com/company/groomingzone/AbstractRestDocsController.java
@@ -0,0 +1,47 @@
+package com.company.groomingzone;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+public class AbstractRestDocsController {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    protected WebApplicationContext webApplicationContext;
+
+    @Autowired
+    protected RestDocumentationContextProvider restDocumentationContextProvider;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentationContextProvider)
+                        .operationPreprocessors()
+                        .withRequestDefaults(prettyPrint())
+                        .withResponseDefaults(prettyPrint()))
+                .build();
+    }
+
+    // 공통 유틸리티 메서드
+    protected String toJson(Object object) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(object);
+    }
+}
